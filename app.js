ff91bb81a21536af607c047e24b4df54d5d47da2
geir <geir.hilmersen@protonmail.com>
2024-09-16 15:34:02 +0200

    changin app.js to specify view engine and folder for serving static files adding initial ejs templates for handling lists of links. adding main.css in the public/static folder

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
-const default_routes = require('./routes/default_routes')
+const default_routes = require('./routes/default_routes');
+const path = require('path');
 
 const PORT = process.env.PORT || 80;
 
 
-app.use(default_routes)
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+app.use(default_routes);
 
 app.listen(PORT, startApp);
 
--- controllers/default_controller.js
+++ controllers/default_controller.js
@@ -1,7 +1,18 @@
 const index = (req, res)=> {
-    res.render('../views/index');
+    res.render('../views/index', {
+        links:
+        [
+            {
+                link: 'assignment1',
+                href: 'www.vg.no'
+            },
+            {
+                link: 'assignment2',
+                href: 'www.db.no'
+            }
+        ]
+    });
 }
-
 module.exports={
     index
 }
