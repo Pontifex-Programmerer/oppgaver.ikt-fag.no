707426036b039a041a8923c89b045d3d8a9e28c7
Geir Hilmersen <geir.hilmersen@protonmail.com>
2024-12-18 20:35:01 +0100

    adding alias.js - alias model for mongoose. changing user.js - user model for mongoose to match required attributes in the alias.js adding data folder with json files containing data for the alias database

---
+++ model/alias.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const aliasSchema = new Schema({
+    // alias is a predetermined alias as implemented in active directory
+    alias: {
+        type: String,
+    unique: true,
+    required: true
+    },
+    // User is the user that a student registered
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        unique: true,
+        required: false,
+        default: null
+    },
+    // pool is the assigned pool number on vsphere
+    pool: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    // webhost is the hostIP on our web-server
+    webhost: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    state: {
+        type: String,
+        required: true,
+        enum: ['approval pending', 'active', 'suspended', 'marked for removal']
+    }
+});
+
+const Alias=mongoose.model('Alias', aliasSchema);
+module.exports=Alias;
