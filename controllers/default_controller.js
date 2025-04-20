d6f543831ccf417be2ecade6ec919f0effb005a5
Geir Hilmersen <geir.hilmersen@protonmail.com>
2025-04-20 16:30:23 +0200

    fixing a bug where the /internal-error page didnt render if error was null or undefined. Now it correctly displays -unknown error- instead

--- controllers/default_controller.js
+++ controllers/default_controller.js
@@ -3,6 +3,7 @@ const User = require('../models/Student');
 const httpFeedbackHandler = require('../handlers/httpFeedbackHandler');
 
 const internalError = (req,res)=>{
+    let error = null;
     res.render('internalError', {error});
 }
 
